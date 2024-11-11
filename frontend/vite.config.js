607e16732b6c14071dd86508b8fab6fd3031ef0e
pulemashile <166323606+pulemashile@users.noreply.github.com>
2024-11-11 11:47:45 +0200

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the build output goes to 'dist' directory
-  },
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist', // Ensure the build output goes to 'dist' directory
+//   },
+// })
